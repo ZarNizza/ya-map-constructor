e05b2c50791cd9f677187193fd711d915bd47766
ZarNizza <gormaster@mail.ru>
2021-12-25 19:43:38 +0400

    initial, old class-based App

--- src/RoutesConstructor/RoutesConstructor.js
+++ src/RoutesConstructor/RoutesConstructor.js
@@ -1,141 +1,147 @@
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import RoutesConstructorMap from './RoutesConstructorMap';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import RoutesConstructorMap from "./RoutesConstructorMap";
 import {
-	SortableContainer,
-	SortableElement,
-	SortableHandle,
-	arrayMove
-} from 'react-sortable-hoc';
-import './RoutesConstructor.css';
+  SortableContainer,
+  SortableElement,
+  SortableHandle,
+  arrayMove,
+} from "react-sortable-hoc";
+import "./RoutesConstructor.css";
 
 class RoutesConstructor extends Component {
-	__resizeTimeout=0;
+  __resizeTimeout = 0;
 
-	constructor(props){
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state={
-			points:props.points||[]
-		};
+    this.state = {
+      points: props.points || [],
+    };
 
-		this.createMarker=this.createMarker.bind(this);
-	}
+    this.createMarker = this.createMarker.bind(this);
+  }
 
-	render(){
-		const DragHandle=SortableHandle(
-				({title})=><span className="title">{title}</span>
-			),
-			SortableItem=SortableElement(
-				({item})=>
-					<div className="point-list__item">
-						<DragHandle title={item.title}/>
-						<span className="remove" onClick={(e)=>this.removeMarker(item.id,e)}/>
-					</div>
-			),
-			SortableList=SortableContainer(
-				({points})=>
-					<div className="point-list">
-						{points.map((item)=>(
-							<SortableItem key={item.id} index={item.index} item={item}/>
-						))}
-					</div>
-			);
+  render() {
+    const DragHandle = SortableHandle(({ title }) => (
+        <span className="title">{title}</span>
+      )),
+      SortableItem = SortableElement(({ item }) => (
+        <div className="point-list__item">
+          <DragHandle title={item.title} />
+          <span
+            className="remove"
+            onClick={(e) => this.removeMarker(item.id, e)}
+          />
+        </div>
+      )),
+      SortableList = SortableContainer(({ points }) => (
+        <div className="point-list">
+          {points.map((item) => (
+            <SortableItem key={item.id} index={item.index} item={item} />
+          ))}
+        </div>
+      ));
 
-		this.handleWinResize();
+    this.handleWinResize();
 
-		return (
-			<div className="routes-constructor" ref="routesConstructor">
-				<div className="left-side">
-					<form onSubmit={this.createMarker} method="post">
-						<div>
-							<div className="input">
-								<input type="text"
-								       ref="markerName"
-								       defaultValue=""
-								       placeholder="Новая точка маршрута"/>
-							</div>
-							<SortableList
-								points={this.state.points}
-								onSortEnd={this.onSortEnd}
-								useDragHandle={true}/>
-						</div>
-					</form>
-				</div>
-				<div className="right-side">
-					<RoutesConstructorMap markers={this.state.points}/>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className="routes-constructor" ref="routesConstructor">
+        <div className="left-side">
+          <form onSubmit={this.createMarker} method="post">
+            <div>
+              <div className="input">
+                <input
+                  type="text"
+                  ref="markerName"
+                  defaultValue=""
+                  placeholder="Новая точка маршрута"
+                />
+              </div>
+              <SortableList
+                points={this.state.points}
+                onSortEnd={this.onSortEnd}
+                useDragHandle={true}
+              />
+            </div>
+          </form>
+        </div>
+        <div className="right-side">
+          <RoutesConstructorMap markers={this.state.points} />
+        </div>
+      </div>
+    );
+  }
 
-	onSortEnd=({oldIndex,newIndex})=>{
-		this.setState({
-			points:arrayMove(this.state.points,oldIndex,newIndex)
-				.map((item,i)=>{
-					item.index=i;
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    this.setState({
+      points: arrayMove(this.state.points, oldIndex, newIndex).map(
+        (item, i) => {
+          item.index = i;
 
-					return item;
-				})
-		});
-	};
+          return item;
+        }
+      ),
+    });
+  };
 
-	componentDidMount(){
-		this.handleWinResize();
+  componentDidMount() {
+    this.handleWinResize();
 
-		window.addEventListener('resize',this.handleWinResize.bind(this));
-	}
-	componentWillUnmount(){
-		window.removeEventListener('resize',this.handleWinResize.bind(this));
-	}
+    window.addEventListener("resize", this.handleWinResize.bind(this));
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWinResize.bind(this));
+  }
 
-	handleWinResize(){
-		clearTimeout(this.__resizeTimeout);
+  handleWinResize() {
+    clearTimeout(this.__resizeTimeout);
 
-		this.__resizeTimeout=setTimeout(()=>{
-			let Node=ReactDOM.findDOMNode(this.refs.routesConstructor),
-				Style=Node.style;
+    this.__resizeTimeout = setTimeout(() => {
+      let Node = ReactDOM.findDOMNode(this.refs.routesConstructor),
+        Style = Node.style;
 
-			Style.cssText='width:100vw;height:100vh;';
-			if(Style.height!=='100vh')
-				Style.cssText=`width:${document.body.offsetWidth}px;`
-					+`height:${document.body.offsetHeight}px;`;
+      Style.cssText = "width:100vw;height:100vh;";
+      if (Style.height !== "100vh")
+        Style.cssText =
+          `width:${document.body.clientWidth}px;` +
+          `height:${document.body.clientHeight}px;`;
 
-			let cont=Node.querySelector('.left-side'),
-				pointList=cont.querySelector('.point-list');
+      let cont = Node.querySelector(".left-side"),
+        pointList = cont.querySelector(".point-list");
 
-			pointList.style.height=(Node.clientHeight-cont.clientHeight+pointList.clientHeight)+'px';
-		},50);
-	}
+      pointList.style.height =
+        Node.clientHeight - cont.clientHeight + pointList.clientHeight + "px";
+    }, 50);
+  }
 
-	createMarker(e){
-		e.preventDefault();
+  createMarker(e) {
+    e.preventDefault();
 
-		var Node=ReactDOM.findDOMNode(this.refs.markerName);
-		if(!Node.value.trim().length)
-			return;
+    var Node = ReactDOM.findDOMNode(this.refs.markerName);
+    if (!Node.value.trim().length) return;
 
-		var Point={
-			id:(new Date()).getTime()+Math.random(),
-			title:Node.value.trim(),
-			position:window.__Map.getCenter(),
-			index:this.state.points.length
-		};
+    var Point = {
+      id: new Date().getTime() + Math.random(),
+      title: Node.value.trim(),
+      position: window.__Map.getCenter(),
+      index: this.state.points.length,
+    };
 
-		this.setState({
-			points:this.state.points.concat(Point)
-		});
+    this.setState({
+      points: this.state.points.concat(Point),
+    });
 
-		Node.value='';
-	}
+    Node.value = "";
+  }
 
-	removeMarker(id,e){
-		e.stopPropagation();
+  removeMarker(id, e) {
+    e.stopPropagation();
 
-		this.setState({
-			points:this.state.points.filter(item=>item.id!==id)
-		});
-	}
+    this.setState({
+      points: this.state.points.filter((item) => item.id !== id),
+    });
+  }
 }
 
 export default RoutesConstructor;
--- src/RoutesConstructor/RoutesConstructorMap.js
+++ src/RoutesConstructor/RoutesConstructorMap.js
@@ -1,132 +1,140 @@
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-class RoutesConstructorMap extends Component{
-	__state=0;
-	__Markers=[];
-	__Map={};
-	__Path;
+class RoutesConstructorMap extends Component {
+  __state = 0;
+  __Markers = [];
+  __Map = {};
+  __Path;
 
-	static propTypes={
-		markers:PropTypes.arrayOf(PropTypes.shape({
-			id:PropTypes.number.isRequired,
-			title:PropTypes.string.isRequired,
-			position:PropTypes.array.isRequired
-		})).isRequired
-	};
+  static propTypes = {
+    markers: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        position: PropTypes.array.isRequired,
+      })
+    ).isRequired,
+  };
 
-	constructor(props){
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state={
-			markers:props.markers||[]
-		};
+    this.state = {
+      markers: props.markers || [],
+    };
 
-		this.__init();
-	}
+    this.__init();
+  }
 
-	render(){
-		var indexedMarkers={},
-			indexedPoints={},
-			points=[].slice.call(this.props.markers);
+  render() {
+    var indexedMarkers = {},
+      indexedPoints = {},
+      points = [].slice.call(this.props.markers);
 
-		this.__Markers.forEach((item,i)=>indexedMarkers[item.id]=this.__Markers[i]);
-		points.forEach((item,i)=>indexedPoints[item.id]=points[i]);
+    this.__Markers.forEach(
+      (item, i) => (indexedMarkers[item.id] = this.__Markers[i])
+    );
+    points.forEach((item, i) => (indexedPoints[item.id] = points[i]));
 
-		this.__Markers=this.__Markers.filter(item=>{
-			if(!!indexedPoints[item.id])
-				return true;
+    this.__Markers = this.__Markers
+      .filter((item) => {
+        if (!!indexedPoints[item.id]) return true;
 
-			this.__Map.geoObjects.remove(item.marker);
+        this.__Map.geoObjects.remove(item.marker);
 
-			return false;
-		}).map(item=>{
-			item.index=indexedPoints[item.id].index;
+        return false;
+      })
+      .map((item) => {
+        item.index = indexedPoints[item.id].index;
 
-			return item;
-		});
+        return item;
+      });
 
-		points.filter(item=>!indexedMarkers[item.id]).forEach(item=>{
-			item.marker=new window.ymaps.Placemark(item.position,{
-				balloonContent:item.title
-			},{
-				draggable:true
-			});
+    points
+      .filter((item) => !indexedMarkers[item.id])
+      .forEach((item) => {
+        item.marker = new window.ymaps.Placemark(
+          item.position,
+          {
+            balloonContent: item.title,
+          },
+          {
+            draggable: true,
+          }
+        );
 
-			item.marker.events.add('dragend',(e)=>{
-				item.position=e.get('target').geometry.getCoordinates();
-				this.renderPath();
-			});
+        item.marker.events.add("dragend", (e) => {
+          item.position = e.get("target").geometry.getCoordinates();
+          this.renderPath();
+        });
 
-			this.__Map.geoObjects.add(item.marker);
-			this.__Markers.push(item);
-		});
+        this.__Map.geoObjects.add(item.marker);
+        this.__Markers.push(item);
+      });
 
-		this.renderPath();
+    this.renderPath();
 
-		return <div className="map" ref="routesConstructorMap"></div>;
-	}
+    return <div className="map" ref="routesConstructorMap"></div>;
+  }
 
-	renderPath(){
-		if(this.__state!==2)
-			return;
+  renderPath() {
+    if (this.__state !== 2) return;
 
-		const Polyline=new window.ymaps.Polyline(
-			this.__Markers
-				.sort((a,b)=>a.index-b.index)
-				.map(item=>item.position),
-			{},
-			{
-				strokeColor:"#000000",
-				strokeWidth:4,
-				strokeOpacity:0.5
-			}
-		);
+    const Polyline = new window.ymaps.Polyline(
+      this.__Markers
+        .sort((a, b) => a.index - b.index)
+        .map((item) => item.position),
+      {},
+      {
+        strokeColor: "#000000",
+        strokeWidth: 4,
+        strokeOpacity: 0.5,
+      }
+    );
 
-		if(this.__Path)
-			this.__Map.geoObjects.remove(this.__Path);
-		
-		this.__Map.geoObjects.add(this.__Path=Polyline);
-	}
+    if (this.__Path) this.__Map.geoObjects.remove(this.__Path);
 
-	componentDidMount(){
-		window.addEventListener('resize',this.handleWinResize);
-	}
-	componentWillUnmount(){
-		window.removeEventListener('resize',this.handleWinResize);
-	}
+    this.__Map.geoObjects.add((this.__Path = Polyline));
+  }
 
-	getCenter(){
-		return this.__state===2?this.__Map.getCenter():[0,0];
-	}
+  componentDidMount() {
+    window.addEventListener("resize", this.handleWinResize);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWinResize);
+  }
 
-	__init(){
-		if(this.__state)
-			return void 0;
+  getCenter() {
+    return this.__state === 2 ? this.__Map.getCenter() : [53.23, 50.16];
+  }
 
-		var That=this;
-		this.__state=1;
+  __init() {
+    if (this.__state) return void 0;
 
-		window.ymaps.ready(function(){
-			window.__Map=That.__Map=new window.ymaps.Map(ReactDOM.findDOMNode(That.refs.routesConstructorMap),{
-				center:That.getCenter(),
-				zoom:10
-			});
-			That.__state=2;
+    var That = this;
+    this.__state = 1;
 
-			window.ymaps.geocode('Москва').then(function(res){
-				That.__Map.setCenter(
-					res.geoObjects.get(0).geometry.getCoordinates()
-				);
-			});
-		});
-	}
+    window.ymaps.ready(function () {
+      window.__Map = That.__Map = new window.ymaps.Map(
+        ReactDOM.findDOMNode(That.refs.routesConstructorMap),
+        {
+          center: That.getCenter(),
+          zoom: 10,
+        }
+      );
+      That.__state = 2;
 
-	handleWinResize(){
-		if(this.__state===2)
-			this.__Map.container.fitToViewport();
-	}
+      window.ymaps.geocode("Самара").then(function (res) {
+        That.__Map.setCenter(res.geoObjects.get(0).geometry.getCoordinates());
+      });
+    });
+  }
+
+  handleWinResize() {
+    if (this.__state === 2) this.__Map.container.fitToViewport();
+  }
 }
 
 export default RoutesConstructorMap;
